3fba7abe079440daf886d56ef7af3f0c30e7ea5b
mblancosoto <mblancosoto@victoria24.danielcastelao.org>
2016-11-03 09:02:36 +0100

    Merge origin/v2
    Conflicts:
    	src/otraprueba/Otraprueba.java

---
+++ src/boletin7c/Boletin7c.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin7c;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mblancosoto
+ */
+public class Boletin7c {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        
+        int select;
+
+        Figures obj = new Figures();
+  
+        
+       
+        
+
+             
+           select = Integer.parseInt(JOptionPane.showInputDialog("\n teclea 1 para el circulo \n teclea 2 para el cuadrado \n teclea 3 para el triangulo \n teclea 0 para salir "));
+                 
+      switch(select){
+          
+          case 1 : obj.circulo(0);
+          break;
+          case 2 : obj.cuadrado(0);
+          break;
+          case 3 : obj.triangulo(0, 0);
+          break;
+              
+          default:
+              JOptionPane.showMessageDialog(null, "Error,seleccione otro número ");
+        
+      }
+        
+    }
+    
+}
---
+++ src/boletin7c/Figures.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin7c;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mblancosoto
+ */
+public class Figures {
+    
+      private float lado,base,altura,radio,triangulo,cuadrado,circulo;
+    private final double Pi=3.14;
+    
+    
+    
+    public Figures(){
+        
+        
+    }
+    
+    public Figures(float lado,float base,float altura,float radio){
+        this.lado = lado;
+        this.base = base;
+        this.altura = altura;
+        this.radio = radio;
+       
+    }
+    
+    
+    
+    public void cuadrado(float lado){
+        lado = Float.parseFloat(JOptionPane.showInputDialog(" inserte el lado"));
+        JOptionPane.showMessageDialog(null,"el area del cuadrado es " +lado*lado);
+    }
+    
+    public void circulo (float radio){
+        radio = Float.parseFloat(JOptionPane.showInputDialog(" Inserte el radio "));
+        JOptionPane.showMessageDialog(null," el area de la circunferencia es "+ Pi*Math.pow(radio,2));
+    }
+    
+    
+    public void triangulo(float base,float altura){
+        base = Float.parseFloat(JOptionPane.showInputDialog(" inserte la base "));
+        altura = Float.parseFloat(JOptionPane.showInputDialog(" Inserte la altura " ));
+        JOptionPane.showMessageDialog(null," el area del triangulo es "+ (base*altura)/2);
+        
+    }
+    
+    
+    
+    
+}
